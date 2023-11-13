984cbb4fc2eb3989b41abe825d0b6fa3028f96fb
ezequelyson <134976959+ezequelyson@users.noreply.github.com>
2023-11-13 20:08:28 -0300

    finalizando a primeira aula

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ console.log(document.getElementById("novoItem"))
 
 form.eddEventListener("submite,evento") => {
      evento.preventdefault()
-     
-    console.log("funcionol")
+
+
+    console.log(evento)
+    console.log(evento.target.elements["nome"].value)
+    console.log(evento.target.elements["quantidade"].value)
 })
